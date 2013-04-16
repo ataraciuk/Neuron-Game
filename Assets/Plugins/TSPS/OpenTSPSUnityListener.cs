b0275415bd4708cea4a1e1b3bcd7bf44b281ddeb
ataraciuk <ataraciuk@gmail.com>
2013-04-16 00:06:23 -0400

    changed motion, hip movement is required to move ball

--- Assets/Plugins/TSPS/OpenTSPSUnityListener.cs
+++ Assets/Plugins/TSPS/OpenTSPSUnityListener.cs
@@ -34,10 +34,11 @@ using System.Linq;
 
 public class OpenTSPSUnityListener : MonoBehaviour  {
 	
-	private float torsoXMultiplier = 30.0f;
-	private float torsoYMultiplier = 50.0f;
 	private float torsoXBeginning = -1.0f;
 	private float torsoYBeginning = -1.0f;
+	private float l_hip = -1.0f;
+	private float r_hip = -1.0f;
+	private float hipCalibrated = -1.0f;
 	
 	public void OnEnable(){
 		UnityOSCReceiver.OSCMessageReceived += new UnityOSCReceiver.OSCMessageReceivedHandler(OSCMessageReceived);
@@ -46,6 +47,17 @@ public class OpenTSPSUnityListener : MonoBehaviour  {
 	public void OnDisable(){
 		UnityOSCReceiver.OSCMessageReceived -= new UnityOSCReceiver.OSCMessageReceivedHandler(OSCMessageReceived);
 		
+	}
+	
+	public void Update(){
+		if(l_hip >= 0.0f && r_hip >= 0.0f) {
+			var diff = (l_hip + r_hip) / 2;
+			if(hipCalibrated < 0.0f) {
+				hipCalibrated = diff;
+			}
+			BroadcastMessage("HipsMoved", diff - hipCalibrated, SendMessageOptions.DontRequireReceiver);
+			hipCalibrated = diff;
+		}
 	}
 	
 	public void OSCMessageReceived(OSC.NET.OSCMessage message){
@@ -60,11 +72,17 @@ public class OpenTSPSUnityListener : MonoBehaviour  {
 				torsoYBeginning = (float)args[3];
 			}
 			BroadcastMessage("TorsoMoved", new Vector3(
-				((float)args[2] - torsoXBeginning) * torsoXMultiplier,
-				(torsoYBeginning - (float)args[3]) * torsoYMultiplier,
+				((float)args[2] - torsoXBeginning),
+				(torsoYBeginning - (float)args[3]),
 				0), SendMessageOptions.DontRequireReceiver);
 			torsoXBeginning = (float)args[2];
 			torsoYBeginning = (float)args[3];
+		}
+		if(address.StartsWith("/joint") && args[0].ToString() == "l_hip") {
+			l_hip = (float)args[2];
+		}
+		if(address.StartsWith("/joint") && args[0].ToString() == "r_hip") {
+			r_hip = (float)args[2];
 		}
 		//Debug.Log("mouth width: "+mouthWidth+"    mouth height: "+mouthHeight);
 	}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,23 +1,52 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Move : MonoBehaviour {
 	
 	private Vector3 Speed = Vector3.forward / 5;
 	private Vector3 startPos;
+	private float torsoZMultiplier = 200.0f;
+	private float torsoYMultiplier = 50.0f;
+	private float alpha = 0.4f;
+	private List<float> speeds = new List<float>();
+	private List<float> velocities = new List<float>();
+	private int sampleBufferSize = 40;
 		
 	// Use this for initialization
 	void Start () {
 		startPos = this.transform.position;
+		for(int i = 0; i < sampleBufferSize; i++) {
+			speeds.Add(0.0f);
+			velocities.Add(0.0f);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position += Speed;
+		//this.transform.position += Speed;
 		//this.transform.position += Vector3.up * ( Mathf.Sin( Time.fixedTime * 1f ) * 0.3f);
 	}
 	
 	void TorsoMoved(Vector3 torsoPos) {
-		this.transform.position += torsoPos;
+//		var absx = Mathf.Abs(torsoPos.x);
+//		//absx = alpha * absx + (1 - alpha) * backoff;
+//		speeds.RemoveAt(0);
+//		velocities.RemoveAt(0);
+//		speeds.Add(absx);
+//		velocities.Add(torsoPos.x);
+//		float mappedSpeed = Mathf.Log(1+ (speeds.Average() - Mathf.Abs(velocities.Average())));
+//		this.transform.position += new Vector3(0, /*torsoPos.y * torsoYMultiplier*/ 0, mappedSpeed * torsoZMultiplier);
+	}
+	void HipsMoved(float val) {
+		var absx = Mathf.Abs(val);
+		//absx = alpha * absx + (1 - alpha) * backoff;
+		speeds.RemoveAt(0);
+		velocities.RemoveAt(0);
+		speeds.Add(absx);
+		velocities.Add(val);
+		float mappedSpeed = Mathf.Log(1+ (speeds.Average() - Mathf.Abs(velocities.Average())));
+		this.transform.position += new Vector3(0, /*torsoPos.y * torsoYMultiplier*/ 0, mappedSpeed * torsoZMultiplier);		
 	}
 }
