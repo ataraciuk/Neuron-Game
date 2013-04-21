229bf7cddb288282cd70f24eca44ead5f915de60
ataraciuk <ataraciuk@gmail.com>
2013-04-21 18:40:07 -0400

    path based on spline and BDNF randomized on path

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,7 +7,7 @@ public class Move : MonoBehaviour {
 	
 	private Vector3 Speed = Vector3.forward / 5;
 	private Vector3 startPos;
-	private float hipsZMultiplier = 200.0f;
+	private float hipsZMultiplier = 0.2f;
 	private float torsoYMultiplier = 50.0f;
 	private float alpha = 0.4f;
 	private List<float> speeds = new List<float>();
@@ -24,6 +24,15 @@ public class Move : MonoBehaviour {
 	private float jumpingSpeed = 0.0f;
 	private float endJumpTime = 0.0f;
 	private bool endedJump = false;
+	
+	public Transform BDNF; 
+	
+	private float pathCompletion = 0.0f;
+	private IEnumerable<Vector3> thePath;
+	
+	private int BDNFAmount = 10;
+	private float firstBDNFPath = 0.05f;
+	private float lastBDNFPath = 0.95f;
 		
 	// Use this for initialization
 	void Start () {
@@ -35,6 +44,31 @@ public class Move : MonoBehaviour {
 		for(int i = 0; i < jumpLongBufferSize; i++) {
 			torsoPos.Add(0.0f);
 		}
+		Vector3[] path = {
+			new Vector3(976.656f, 1160.348f, 57.45f),
+			new Vector3(20.833f,	972.301f,	430.565f),
+			new Vector3(-930.696f,	0.0f,	176.947f),
+			new Vector3(-383.768f,	0.0f,	-489.477f),
+			new Vector3(-158.563f,	335.323f,	75.834f),
+			new Vector3(714.683f,	274.965f,	-402.153f),
+			new Vector3(-140.179f,	0.0f,	-843.371f),
+			new Vector3(737.663f,	-176.377f,	-1022.617f)/*
+			new Vector3(-930.696f,	0.0f,	176.947f),
+			new Vector3(-930.696f,	0.0f,	176.947f),
+			new Vector3(-930.696f,	0.0f,	176.947f),
+			new Vector3(-930.696f,	0.0f,	176.947f),
+			new Vector3(-930.696f,	0.0f,	176.947f),
+			new Vector3(-930.696f,	0.0f,	176.947f)*/		
+		};
+		path = path.Select(x => x * 0.1f).ToArray();
+		thePath = Interpolate.NewCatmullRom(path, 1000, false);
+		this.transform.position = path[0];
+		Instantiate(BDNF, CRSpline.InterpConstantSpeed(thePath.ToArray(), firstBDNFPath),Quaternion.identity);
+		Instantiate(BDNF, CRSpline.InterpConstantSpeed(thePath.ToArray(), lastBDNFPath),Quaternion.identity);
+		var spread = (lastBDNFPath - firstBDNFPath) / BDNFAmount;
+		for(int i = 1; i <= BDNFAmount - 2; i++) {
+			Instantiate(BDNF, CRSpline.InterpConstantSpeed(thePath.ToArray(), firstBDNFPath + spread * i + (Random.value - 0.5f) * spread * 0.6f), Quaternion.identity);
+		}
 	}
 	
 	// Update is called once per frame
@@ -43,8 +77,7 @@ public class Move : MonoBehaviour {
 			jumping = false;
 			endedJump = false;
 		}
-		//this.transform.position += Speed;
-		//this.transform.position += Vector3.up * ( Mathf.Sin( Time.fixedTime * 1f ) * 0.3f);
+		//pathCompletion += 0.0001f;
 	}
 	
 	void TorsoMoved(float val) {
@@ -65,7 +98,11 @@ public class Move : MonoBehaviour {
 			velocities.Add(val);
 		}
 		float mappedSpeed = Mathf.Log(1+ (speeds.Average() - Mathf.Abs(velocities.Average())));
-		this.transform.position += new Vector3(0, /*torsoPos.y * torsoYMultiplier*/ 0, mappedSpeed * hipsZMultiplier);
+		this.pathCompletion += mappedSpeed * hipsZMultiplier;
+		pathCompletion = Mathf.Min(pathCompletion, 1.0f);
+		Debug.Log(pathCompletion);
+		this.transform.position += CRSpline.InterpConstantSpeed(thePath.ToArray(), pathCompletion) - this.transform.position;
+		//this.transform.position += new Vector3(0, /*torsoPos.y * torsoYMultiplier*/ 0, mappedSpeed * hipsZMultiplier);
 	}
 	
 	void UpdateJumpThreshold(float val){
@@ -75,8 +112,9 @@ public class Move : MonoBehaviour {
 	
 	void OnJump() {
 		if(!jumping) {
+			Debug.Log("JUMP!");
 			jumping = true;
-			iTween.MoveAdd(this.gameObject, iTween.Hash(
+			iTween.MoveBy(this.gameObject, iTween.Hash(
 				"amount", Vector3.up * jumpHeight,
 				"time", jumpTime,
 				"easetype", iTween.EaseType.easeOutQuad,
@@ -87,7 +125,7 @@ public class Move : MonoBehaviour {
 	}
 	
 	void Fall() {
-		iTween.MoveAdd(this.gameObject, iTween.Hash(
+		iTween.MoveBy(this.gameObject, iTween.Hash(
 			"amount", Vector3.down * jumpHeight,
 			"time", jumpTime,
 			"easetype", iTween.EaseType.easeInQuad,
