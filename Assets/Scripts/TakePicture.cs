97f4121b8a7a7f900ec318b497b4389ce01ddca2
ataraciuk <ataraciuk@gmail.com>
2013-05-01 02:46:00 -0400

    showing tumbnails of images during gameplay

--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TakePicture : MonoBehaviour {
 	
@@ -8,11 +9,15 @@ public class TakePicture : MonoBehaviour {
 	
     // For saving to the _savepath
     private string _SavePath = "/Users/Shared/Unity/"; //Change the path here!
-    int _CaptureCounter = 0;
+	private List<Texture2D> pics = new List<Texture2D>();
+	private const int xSize = 400;
+	private const int ySize = 300;
+	private const float xTumb = 80.0f;
+	private const float yTumb = (float) ySize * xTumb / (float) xSize;
 	
 	// Use this for initialization
 	void Start () {
-		wct = new WebCamTexture(WebCamTexture.devices[0].name, 400, 300);
+		wct = new WebCamTexture(WebCamTexture.devices[0].name, xSize, ySize);
 		wct.Play();
 	}
 	
@@ -27,7 +32,15 @@ public class TakePicture : MonoBehaviour {
        snap.SetPixels(wct.GetPixels());
        snap.Apply();
 
-       System.IO.File.WriteAllBytes(_SavePath + _CaptureCounter.ToString() + ".png", snap.EncodeToPNG() );
-       ++_CaptureCounter;
+       System.IO.File.WriteAllBytes(_SavePath + pics.Count.ToString() + ".png", snap.EncodeToPNG() );
+       pics.Add(snap);
     }
+	
+	void OnGUI(){
+		float starting = 8.0f, yPos = 0.0f;
+		pics.ForEach(delegate(Texture2D pic){
+			GUI.DrawTexture(new Rect(Screen.width - xTumb - starting, yPos + starting, xTumb, yTumb), pic);
+			yPos += yTumb + starting;
+		});
+	}
 }
